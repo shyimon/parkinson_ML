cf0485df92b30754a893d1796495ec8bc6a7bddc
iamalessiastocco <alessiastocco01@gmail.com>
2025-11-11 16:37:46 +0100

    prova alessia

---
+++ Monk_Alessia/monk_ale.py
@@ -0,0 +1,15 @@
+from ucimlrepo import fetch_ucirepo 
+  
+# fetch dataset 
+monk_s_problems = fetch_ucirepo(id=70) 
+  
+# data (as pandas dataframes) 
+X = monk_s_problems.data.features 
+y = monk_s_problems.data.targets 
+  
+# metadata 
+print(monk_s_problems.metadata) 
+
+# variable information 
+print(monk_s_problems.variables) 
+
