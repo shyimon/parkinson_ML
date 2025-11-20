9ad8fe384cd1c6d7b798630bbdd91d32bb52fb23
shyimon <botticelli.cosimo@gmail.com>
2025-11-20 11:08:59 +0100

    Added backprop (not working)

--- Monk_Cosimo/neural_network.py
+++ Monk_Cosimo/neural_network.py
@@ -1,10 +1,12 @@
 import numpy as np
 import sys
+from tqdm import tqdm
 np.set_printoptions(precision=3, suppress=True)
 
 class neural_network:
     def __init__(self):
         self.network_structure = []
+        self.activations = []
         
     def add_layer(self, weights):
         if self.network_structure and len(self.network_structure[-1][1]) != len(weights):
@@ -20,12 +22,17 @@ class neural_network:
     def __tanh_activation(self, input):
         return((np.exp(input) - np.exp(-input)) / (np.exp(input) + np.exp(-input)))
     
+    def __tanh_deriv(self, input):
+        return 1 - np.square(input)
+    
     def feed_forward(self, input_layer):
+        self.activations = [input_layer]
         feeding = np.array(input_layer)
         for hidden_layer in self.network_structure:
             hidden_layer = np.array(hidden_layer)
             feeding = feeding.dot(hidden_layer)
             feeding = self.__tanh_activation(feeding)
+            self.activations.append(feeding)
         return feeding
     
     def MSE_loss(self, output, Y, total_examples):
@@ -33,11 +40,26 @@ class neural_network:
         s = np.sum(s)/total_examples
         return(s)
     
-    def backprop(self, inputs, target, learning_rate=1):
-        for layer in range(len(self.network_structure)):
-            error = target - float(self.feed_forward(inputs))
-            print(f"Predicted value is {self.feed_forward(inputs)}, while real value was {target}: error is {error}")
+    def backprop(self, target, learning_rate=0.01):
+        target = np.array(target).reshape(1, -1)
+        inputs = np.array(self.activations[-1]).reshape(1, -1)
+        error = target - float(self.activations[-1])
 
+        for layer in range(len(self.network_structure), 0, -1):
+            print(f"Layer {layer}")
+            delta = error * self.__tanh_deriv(self.activations[layer])
+            previous_layer_error = np.dot(delta, self.network_structure[layer - 1].T)
+            print(len(self.activations))
+            print(f"activ \n{self.activations[layer - 1]}")
+            print(f"delta {delta}")
+            
+            gradient = np.dot(self.activations[layer - 1].T, delta)
+            # print(self.network_structure)
+            self.network_structure[layer] += learning_rate * gradient
+            # print(self.network_structure)
+            error = previous_layer_error
+        
+    
 
     # The training loop. It takes in input:
     # - x, the inputs
@@ -50,11 +72,12 @@ class neural_network:
         loss = []
         for ep in range(epochs):
             epoch_losses = []
-            for input in range(len(x)):
-                output = self.feed_forward(input)
+            for input in range(1):
+                output = self.feed_forward(x.iloc[input])
                 epoch_losses.append(self.MSE_loss(output, Y[input], len(Y)))
-                self.network_structure = self.backprop(input, Y[input])
-            print(f"Accuracy for epoch {ep+1} = {(1-(sum(epoch_losses)/len(x))*100)}")
+                # self.network_structure = self.backprop(x.iloc[input], Y[input])
+                self.backprop(Y[input])
+            # print(f"Accuracy for epoch {ep+1} = {(1-(sum(epoch_losses)/len(x))*100)}")
             accuracy.append(1-(sum(epoch_losses)/len(x))*100)
             loss.append(sum(epoch_losses) / len(x))
         return(self.network_structure, accuracy, loss)
